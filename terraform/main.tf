58d437bd4c3c26d541bddd446a346eebe3bca74e
raisalsalim <raisalsalim333@gmail.com>
2024-05-27 10:31:49 +0530

    Initial commit with .gitignore to exclude .terraform

---
+++ terraform/main.tf
@@ -0,0 +1,45 @@
+provider "aws" {
+  region = "ap-south-1"  # Update with your preferred region
+}
+
+resource "aws_security_group" "allow_web" {
+  name        = "allow_web"
+  description = "Allow web traffic"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-0f58b397bc5c1f2e8"  # Update with the latest Ubuntu AMI ID for your region
+  instance_type = "t2.micro"
+  security_groups = [aws_security_group.allow_web.name]
+  
+  tags = {
+    Name = "flask-app-instance"
+  }
+
+  key_name = "CI-CD"  # Ensure you have a key pair created in AWS
+}
+
+output "instance_public_dns" {
+  value = aws_instance.web.public_dns
+}
